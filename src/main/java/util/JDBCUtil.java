f117c7bbef0bec61836b0b78e1879a64f0d668cb
ducphat0311@gmail.com <ducphat0311@gmail.com>
2026-03-10 22:06:38 +0700

    [DB]: tạo kết nối DB

---
+++ src/main/java/util/JDBCUtil.java
@@ -0,0 +1,25 @@
+package util;
+
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class JDBCUtil {
+
+    private static DataSource ds;
+
+    static {
+        try {
+            Context ctx = new InitialContext();
+            ds = (DataSource) ctx.lookup("java:comp/env/jdbc/mydb");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() throws Exception {
+        return ds.getConnection();
+    }
+}
