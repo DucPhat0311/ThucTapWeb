dcc8b6b30af9e79c51634288416366e94f20e8ba
23130087-AnhHan <23130087@st.hcmuaf.edu.vn>
2026-03-10 21:21:19 +0700

    [FEAT]: Tạo Class Shipping.java cho model

---
+++ src/main/java/model/Shipping.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Shipping {
+	private int spid;
+	private String city;
+	private BigDecimal price;
+	public Shipping(int spid, String city, BigDecimal price) {
+		super();
+		this.spid = spid;
+		this.city = city;
+		this.price = price;
+	}
+	public Shipping() {
+		super();
+	}
+	public int getSpid() {
+		return spid;
+	}
+	public void setSpid(int spid) {
+		this.spid = spid;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	
+	
+}
