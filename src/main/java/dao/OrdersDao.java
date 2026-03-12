63c66b844fb4c91d30361a6a1dc0557fdbb0694f
23130087-AnhHan <23130087@st.hcmuaf.edu.vn>
2026-03-12 22:32:43 +0700

    [FEAT] Thêm hàm insertOrders để tạo đơn hàng mới

--- src/main/java/dao/OrdersDao.java
+++ src/main/java/dao/OrdersDao.java
@@ -1,4 +1,39 @@
 package dao;
 
-public class OrdersDao {
+import model.Order;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class OrdersDao extends BaseDao {
+    public int insertOrders(Order order) {
+        String sql = "INSERT INTO orders(userID,addressID,shipping_fee,note,subtotal,paymentMethod,discountAmount,totalAmount) VALUES(?,?,?,?,?,?,?,?)";
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+
+            ps.setInt(1, order.getUserID());
+            ps.setInt(2, order.getAddressID());
+            ps.setBigDecimal(3, order.getShipping_fee());
+            ps.setString(4, order.getNote());
+            ps.setBigDecimal(5, order.getSubtotal());
+            ps.setString(6, order.getPaymentMethod());
+            ps.setBigDecimal(7, order.getDiscountAmount());
+            ps.setBigDecimal(8, order.getTotalAmount());
+            int affected = ps.executeUpdate();
+
+            if (affected > 0) {
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        return rs.getInt(1);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
 }
