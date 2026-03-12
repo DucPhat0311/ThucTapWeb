310d07974ecac2808d567c47b24144278381468f
ducphat0311@gmail.com <ducphat0311@gmail.com>
2026-03-12 10:14:54 +0700

    [FEAT]: điều hướng sang giao diện about,contact,blog

---
+++ src/main/java/controller/AboutController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/about")
+public class AboutController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public AboutController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        request.setAttribute("active", "about");
+
+        request.getRequestDispatcher("/WEB-INF/views/about.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
