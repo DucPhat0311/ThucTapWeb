2826277682a6a6bc5fc7a53446e451bab1a4c5dc
23130087-AnhHan <23130087@st.hcmuaf.edu.vn>
2026-03-13 22:46:18 +0700

    [FEAT] Thêm chức năng lấy thông tin địa chỉ theo addressID

--- src/main/java/dao/AddressDao.java
+++ src/main/java/dao/AddressDao.java
@@ -117,4 +117,31 @@ public class AddressDao extends BaseDao {
             throw new RuntimeException(e);
         }
     }
+    public Address selectAddressByAddressID(int addressId)  {
+        Address add = new Address();
+        String sql = "select a.addressID,a.fulladdress,s.city_name as city, a.ward,a.phone,a.userID,a.isDefault, a.country from address a "
+                + "JOIN shipping s ON s.city_code = a.city_code where addressID =? ";
+        try( Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);){
+
+            ps.setInt(1, addressId);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+
+                add.setAddressID(rs.getInt("addressID"));
+                add.setFullAddress(rs.getString("fulladdress"));
+                add.setWard(rs.getString("ward"));
+                add.setCity(rs.getString("city"));
+                add.setCountry(rs.getString("country"));
+                add.setPhone(rs.getString("phone"));
+                add.setIsDefault(rs.getBoolean("isDefault"));
+            }
+            return add;
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
