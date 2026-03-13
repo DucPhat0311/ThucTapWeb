b0353e3118d30ddc7e4c72225ac2802453c52136
23130087-AnhHan <23130087@st.hcmuaf.edu.vn>
2026-03-13 20:44:17 +0700

    [FEAT] Thêm cookie ghi nhớ đăng nhập

---
+++ src/main/java/util/CookieUtil.java
@@ -0,0 +1,16 @@
+package util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CookieUtil {
+    private static final String USER_ID = "user_id";
+    private static final String TOKEN = "token_user";
+    private static final String REMEMBER_COOKIE = "remember_me";
+    private static final int COOKIE_AGE = 30 * 24 * 60 * 60; // 30 ngày
+
+
+}
