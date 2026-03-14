ffdd545a4a74fb32f84435ff6b22119e8cc47ce6
23130087-AnhHan <23130087@st.hcmuaf.edu.vn>
2026-03-14 16:10:21 +0700

    [FEAT]: Thêm quản lý lịch sử đơn hàng

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,12 +1,18 @@
 package controller;
 
+import dao.OrdersDao;
+import model.Order;
+import model.UserSession;
+
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/user/*")
 public class UserController extends HttpServlet {
@@ -18,12 +24,14 @@ public class UserController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         String path = request.getPathInfo();
+        HttpSession session = request.getSession();
+        UserSession userSession = (UserSession) session.getAttribute("user");
+        OrdersDao orDao = new OrdersDao();
+        String selected ="";
+        String condition=null;
+        List<Order> listOrder =null;
 
-        if (path == null) {
-            path = "/settings";
-        }
 
         switch (path) {
 
@@ -32,12 +40,38 @@ public class UserController extends HttpServlet {
                         .forward(request, response);
                 break;
 
-            case "/order_history":
-                request.getRequestDispatcher("/WEB-INF/views/order_history.jsp")
-                        .forward(request, response);
-                break;
-
             case "/orders_his":
+
+                condition = request.getParameter("search");
+                if(condition != null) {
+                    switch (condition) {
+                        case "today":
+                            selected = "today";
+                            listOrder = orDao.selectOrdersByToday(userSession.getIdUser());
+                            break;
+                        case "week":
+                            selected = "week";
+
+                            listOrder = orDao.selectOrdersByWeek(userSession.getIdUser());
+                            break;
+                        case "month":
+                            selected = "month";
+                            listOrder = orDao.selectOrdersByMonth(userSession.getIdUser());
+                            break;
+                        case "all":
+                            selected = "all";
+                            listOrder = orDao.selectOrderByUserID(userSession.getIdUser());
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Unexpected value: " + condition);
+                    }
+                }else {
+                    selected = "all";
+                    listOrder = orDao.selectOrderByUserID(userSession.getIdUser());
+                }
+                request.setAttribute("choose", selected);
+                request.setAttribute("orders", listOrder);
+                request.setAttribute("account",4);
                 request.getRequestDispatcher("/WEB-INF/views/orders_his.jsp")
                         .forward(request, response);
                 break;
